aaa487c1d5c0d168b0338c3ac80731dc2b1cd9cb
ilhamadhim <ilhamm179@gmail.com>
2022-11-26 22:22:03 +0700

    fix : update link activity card

--- src/components/ActivityCard.jsx
+++ src/components/ActivityCard.jsx
@@ -13,29 +13,29 @@ import { Link } from "react-router-dom";
 
 function ActivityCard({ index, ...props }) {
   return (
-    <Card data-cy={`activity-item`}>
-      <CardHeader>
-        <Link to={`/detail/${props.id}`}>
+    <Link to={`/detail/${props.id}`}>
+      <Card data-cy={`activity-item`}>
+        <CardHeader>
           <Heading size="sm" data-cy="activity-item-title">
             {props.title}
           </Heading>
-        </Link>
-      </CardHeader>
+        </CardHeader>
 
-      <CardFooter as={Flex} justifyContent="space-between">
-        <Text my="auto" data-cy="activity-item-date">
-          {dateformat(props.created_at, "d mmmm yyyy")}
-        </Text>
-        <Button
-          colorScheme="red"
-          onClick={props.onClickDelete}
-          data-cy="activity-item-delete-button"
-        >
-          {" "}
-          <DeleteIcon />{" "}
-        </Button>
-      </CardFooter>
-    </Card>
+        <CardFooter as={Flex} justifyContent="space-between">
+          <Text my="auto" data-cy="activity-item-date">
+            {dateformat(props.created_at, "d mmmm yyyy")}
+          </Text>
+          <Button
+            colorScheme="red"
+            onClick={props.onClickDelete}
+            data-cy="activity-item-delete-button"
+          >
+            {" "}
+            <DeleteIcon />{" "}
+          </Button>
+        </CardFooter>
+      </Card>
+    </Link>
   );
 }
 
